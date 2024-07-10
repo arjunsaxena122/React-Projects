1c0e6836bfa46603f6c880fd16a438b0b3f9f374
arjunsaxena122 <arjunsaxena122@gmai.com>
2024-07-10 23:21:26 +0530

    Add Second React Project

---
+++ 02- Tailwind Props/Tailwind Props/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='text-white-600 bg-green-600 rounded px-3 py-3'>Hello React</h1>
+    </>
+  )
+}
+
+export default App
